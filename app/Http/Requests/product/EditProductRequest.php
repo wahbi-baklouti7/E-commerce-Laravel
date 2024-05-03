9b1413d7a8606758fe72c278af81bbe50fb00bf3
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2024-05-03 11:32:26 +0100

    Create Product crud

---
+++ app/Http/Requests/product/EditProductRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        // dd($this->product->id);
+        return [
+            'name' => 'required | min:3 |unique:products,name,'.$this->product->id,
+            // 'name' => 'required | min:3 |unique:products,name',
+            'price'=>' numeric | min:0',
+            'description'=>'',
+            'photo'=>' image | mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'category_id'=>'required | exists:App\Models\Category,id',
+        ];
+    }
+}
