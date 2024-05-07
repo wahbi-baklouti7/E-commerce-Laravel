ca98fd2b4971e31baf2d6a8ad4e2fe75d8f9fb92
Wahbi Baklouti <wahbibaklouti7@gmail.com>
2024-05-07 13:39:31 +0100

    add multilanguage

---
+++ lang/ar/message.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+    'products' => 'منتجات',
+    'logout' => 'تسجيل خروج',
+    'login' => 'تسجيل الدخول',
+    'dashboard' => 'لوحة القيادة',
+    'add_category' => 'إضافة فئة ',
+    'categories' => 'فئات',
+    'name' => 'اسم',
+    'image' => 'صورة',
+    'description' => 'وصف',
+    'category' => 'فئة',
+    'created_on' => 'أنشئت في',
+    'price'=>'سعر',
+    'edit' => 'يحرر',
+    'delete' => 'يمسح',
+    'view_archived_products' => 'عرض المنتجات المؤرشفة',
+    'view_archived_categories' => 'عرض الفئات المؤرشفة',
+    'add_product' => 'أضف منتج',
+    'products' => 'منتجات ',
+    'new_orders' => 'طلبات جديدة',
+    'more info' => 'مزيد من المعلومات',
+    'choose_file' => 'اختر ملف',
+    'upload' => 'اختر ملف',
+    'restore' => 'استعادة ',
+    'delete_forever' => 'حذف إلى الأبد',
+    'deleted_on' => 'تم الحذف في',
+    'edit_category' => 'تحرير الفئة',
+    'edit_product' => 'تحرير المنتج',
+    'update' => 'تحرير ',
+    'add' => 'إضافة',
+    'delete_all' => 'حذف الكل  ',
+    'restore_all' => 'استعادة الكل',
+
+    
+    // 'dashboard' => 'لوحة القيادة',
+    // 'dashboard' => 'لوحة القيادة',
+
+];
---
+++ lang/fr/message.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'products' => 'Produits',
+    'dashboard' => 'tableau de bord',
+    'logout' => 'Se déconnecter',
+    'login' => 'Se connecter',
+    'categories' => 'Catégories',
+    'add_category' => 'Ajouter une catégorie',
+    'edit_category' => 'Modifier la catégorie',
+    'edit_product' => 'Modifier le produit',
+    'name' => 'Nom',
+    'image' => 'Image',
+    'description' => 'Description',
+    'category' => 'Catégorie',
+    'created_on' => 'créé à',
+    'edit' => 'Modifier',
+    'delete' => 'Supprimer',
+    'view_archived_products' => 'Voir les produits archivés',
+    'view_archived_categories' => 'voir les catégories archivées',
+    'price' => 'Prix',
+    'add_product' => 'ajouter un produit',
+    'new_orders' => 'nouvelles commandes',
+    'more info' => 'Plus d\'informations',
+    'choose_file' => 'Choisir le fichier',
+    'upload' => 'Télécharger',
+    'restore' => 'Restaurer',
+    'delete_forever' => 'Supprimer définitivement',
+    'deleted_on' => 'Supprimé le',
+    'update' => 'Modifier ',
+    'delete_all' => ' Supprimer tout',
+    'restore_all' => ' Restaurer tout',
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
